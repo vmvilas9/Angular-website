afd84ff36a9c8d2b7240b660228b1884d6580010
Sahil Jain <sahiljainpkl199724@gmail.com>
2018-01-28 20:35:59 +0530

    header responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent
   ],
   imports: [
     BrowserModule
